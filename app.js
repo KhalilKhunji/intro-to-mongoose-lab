639b8a0820112b79301b9d6f6ff2b96d06429c1b
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-22 11:12:24 +0300

    Implemented creating, viewing, updating customers, and quitting app

--- app.js
+++ app.js
@@ -33,19 +33,28 @@ const promptFunction = () => {
     console.log('4. Delete a Customer')
     console.log('5. Quit Tool')
     const option = prompt('Action to perform? ');
-    if (option === 1) {
+    if (option === '1') {
         createCustomer();
     };
-    if (option === 2) {
-
+    if (option === '2') {
+        viewCustomers();
     };
-    if (option === 3) {
-
+    if (option === '3') {
+        updateCustomer();
     };
-    if (option === 4) {
-
+    if (option === '4') {
+        deleteCustomer();
     };
-    if (option === 5) {
+    if (option === '5') {
+        quitFunction();
+    };
+};
+
+const nextPrompt = () => {
+    const nextPrompt = prompt('Return to Menu or Quit? (M/Q) ')
+    if(nextPrompt === 'M') {
+        promptFunction();
+    } else {
         quitFunction();
     };
 };
@@ -59,22 +68,48 @@ const createCustomer = async () => {
     };
     const newCustomer = await customer.create(customerData);
     console.log('New Customer:',newCustomer);
-    const nextPrompt = prompt('Return to Menu or Quit? (M/Q) ')
-    if(nextPrompt === 'M') {
-        promptFunction();
-    };
-    if(nextPrompt === 'Q') {
-        quitFunction();
-    };
+    nextPrompt();
 };
 
+const viewCustomers = async () => {
+    const allCustomers = await customer.find({});
+    console.log('All Customers:', allCustomers);
+    nextPrompt();
+};
 
+const updateCustomer = async () => {
+    const allCustomers = await customer.find({});
+    console.log('All Customers:', allCustomers);
+    const customerToUpdate = prompt('Copy and Paste the ID of the Customer you wish to update: ')
+    const updatingCustomer = await customer.findById(customerToUpdate);
+    console.log('Customer to Update:', updatingCustomer);
+    const customerName = prompt('Updated Name: ');
+    const customerAge = prompt('Updated Age: ');
+    const updatedCustomer = await customer.findByIdAndUpdate(customerToUpdate, {name : customerName, age: Number(customerAge)}, {new: true});
+    console.log('Customer Updated to: ', updatedCustomer);
+    nextPrompt();
+};
 
 
 const quitFunction = () => {
     console.log('Quitting...');
+    mongoose.connection.close();
 };
 
 Init();
 
+// const mongoose = require("mongoose")
+// require("dotenv").config()
+// const prompt = require("prompt-sync")()
 
+// const username = prompt("What is your name? ")
+
+// console.log(`Your name is ${username}`)
+
+// const main = async () => {
+//   await mongoose.connect(process.env.DATABASE_URL)
+//   console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+//   console.log("Welcome to the CRM")
+// }
+
+// main()
